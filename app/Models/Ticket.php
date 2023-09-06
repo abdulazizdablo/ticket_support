6cee1c91e05c37fcba505192b29f2d24f97c40a1
unknown <abooddablo@gmail.com>
2023-09-06 21:06:45 +0300

    first commit

---
+++ app/Models/Ticket.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
+use App\Models\Label;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Ticket extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+
+        'title',
+        'description',
+
+        'priority',
+        'status',
+
+    ];
+
+
+
+    /**
+     * The categories that belong to the Ticket
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+    public function labels(): BelongsToMany
+    {
+        return $this->belongsToMany(Label::class);
+    }
+}
